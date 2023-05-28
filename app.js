016b4e6f533d5e3cd5c7481e810fbbcbd31217f0
hafijurDia <hafijure@gmail.com>
2023-05-28 20:26:38 +0600

    show more button done

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 
 //load function for phones
-const phoneLoad = async(searchText) => {
+const phoneLoad = async(searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhone(data.data);
+    displayPhone(data.data, dataLimit);
     console.log(data);
 }
-
+const showDiv = document.getElementById('show-more');
 //display phone in Ui
-const displayPhone = (phones) => {
+const displayPhone = (phones, dataLimit) => {
     const phoneWrap = document.getElementById('phone-wrap');
     phoneWrap.innerHTML = '';
     //show 9 phones
-    if (phones.length > 9) {
-        const showDiv = document.getElementById('show-more');
+    if (dataLimit && phones.length > 9) {
         phones = phones.slice(0,9);
         showDiv.classList.remove('d-none');
     }else{
         showDiv.classList.add('d-none');
     }
+    //show warning message
     const warningText = document.getElementById('not-found');
     if (phones.length === 0) {
         warningText.classList.remove('d-none');
@@ -47,12 +47,18 @@ const displayPhone = (phones) => {
     toggleSpinner(false);
 }
 
-//search function
-document.getElementById('search-btn').addEventListener('click',function(){
+
+//search process
+const searchProcess = (dataLimit) => {
     //start loader
     toggleSpinner(true);
     const inputValue = document.getElementById('search-field').value;
-    phoneLoad(inputValue);
+    phoneLoad(inputValue, dataLimit);
+}
+
+//search function
+document.getElementById('search-btn').addEventListener('click',function(){
+    searchProcess(9);
 
 })
 
@@ -67,3 +73,9 @@ const toggleSpinner = isLoading => {
     }
 }
 
+//load all phones but its not a best solution
+document.getElementById('btn-show-more').addEventListener('click',function(){
+    searchProcess();
+
+
+})
